b688c57cf5566a9f2e5d3676ba5c27135534d423
Fraser Scott <fraserdscott@gmail.com>
2022-11-07 17:30:00 -0800

    Attempt to browser prove

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import brode from '@geut/esbuild-plugin-brode';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: { plugins: [brode()] }
+  }
 })
